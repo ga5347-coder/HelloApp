44374cc81824203a65942b0164f25ba4badf61db
ga5347-coder <gayathrijyoshika279@gmail.com>
2026-04-05 23:59:18 +0530

    Implemented UC@: Display hello with Command-Line Argument

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -3,11 +3,16 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        System.out.println("Hello and welcome!");
 
-        System.out.println("HelloWorld!");
+        if (args.length > 0) {
+                    String name = args[0];
+                    System.out.println("Hello, " + name + "!");
+        } else {
+                    System.out.println("Hello, World!");
+        }
     }
 }
