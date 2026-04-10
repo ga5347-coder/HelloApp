437f80cedd4545b8e4a8681661cb71fa2c4ab8b7
ga5347-coder <gayathrijyoshika279@gmail.com>
2026-04-10 09:25:30 +0530

    Coded UC3: Display Hello with Command-Line Argument or Default Message

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -5,14 +5,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        if (args.length > 0) {
-                    String name = args[0];
-                    System.out.println("Hello, " + name + "!");
-        } else {
-                    System.out.println("Hello, World!");
-        }
+        // to see how InteliJ IDEA suggests fixing it.
+        String name = "World";
+        if (args.length > 0){
+            name = args[0];
     }
+                    System.out.println("Hello, " + name + "!");
+        }
+
 }
